63917a6c7d4f40b7e69abd9578eb33de94cf2669
Patrick Louis <patrick@psychology.wtf>
2020-10-11 18:15:15 +0300

    Implementation of jpg_to_raw.sh and raw_to_jpg.sh
    raw_to_jpg cannot actually be done with GIMP as it requires interaction.
    As well, jpg to raw in planar mode seems impossible with most tools,
    including imagemagick, so GIMP scripting is necessary.

--- raw_conversion/jpg_to_raw.sh
+++ raw_conversion/jpg_to_raw.sh
@@ -1,6 +1,21 @@
 # Example:
 # ./jpg_to_raw.sh world_map.jpg world_map.data 1
+# 1 for interleaved, 0 for planar
+#
+# Only GIMP can create planar images, even the following only gives interleaved
+# stream -interlace plane -map rgb -storage-type char world_map.jpg stream.planar.data
+# The -interlace isn't respected in imagemagick
 XDG_CONFIG_HOME=$PWD
 export XDG_CONFIG_HOME
 echo $XDG_CONFIG_HOME
-gimp -i -b "(jpg_to_raw \"$1\" \"$2\" #t)" -b '(gimp-quit 0)'
+if [ $# -lt 3 ]
+then
+	echo "$0: image.jpg image.data [1|0, for interleaved or planar]"
+	exit
+fi
+if [ "XX$3XX" == "XX1XX" ]
+then
+	gimp -i -b "(jpg_to_raw \"$1\" \"$2\" #t)" -b '(gimp-quit 0)'
+else
+	gimp -i -b "(jpg_to_raw \"$1\" \"$2\" #f)" -b '(gimp-quit 0)'
+fi
--- raw_conversion/raw_to_jpg.sh
+++ raw_conversion/raw_to_jpg.sh
@@ -1,1 +1,19 @@
-gimp -i -b "(myconvert \"$1\" \"$1.data\")" -b '(gimp-quit 0)'
+# Example:
+# ./raw_to_jpg.sh world_map.data world_map.jpg "2000x1479" 1
+# 1 for interleaved, 0 for planar
+#
+# We can do that using imagemagick but not GIMP because:
+# file-raw-load procedure only allows interactive mode
+# The run mode { RUN-INTERACTIVE (0) }
+if [ $# -lt 4 ]
+then
+	echo "$0: image.jpg image.data widthxheight [1|0, for interleaved or planar]"
+	exit
+fi
+
+if [ "XX$4XX" == "XX1XX" ]
+then
+	convert -size $3 -depth 8 rgb:$1 $2
+else
+	convert -size $3 -interlace plane -depth 8 rgb:$1 $2
+fi
